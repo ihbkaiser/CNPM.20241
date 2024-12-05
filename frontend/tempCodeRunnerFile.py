5ae2a051269e3143e403a47517f70bf4ff88bc27
ihbkaiser <kaiserihb@gmail.com>
2024-12-05 23:53:15 +0700

    del trash

--- frontend/tempCodeRunnerFile.py
+++
@@ -1,3 +0,0 @@
-# Add a label to display password strength
-        # self.password_strength_label = ctk.CTkLabel(self, text="", font=("Arial", 16), text_color="gray")
-        # self.password_strength_label.grid(row=5, column=0, columnspan=2, padx=20, sticky="w")
